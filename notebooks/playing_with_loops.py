c6a5dcd5fdc6a45f4a95e119a9b6b0ba76b20cde
ValentinGehrig <val.gehrig.24@lehre.mosbach.dhbw.de>
2024-12-16 16:35:58 +0100

    Update playing_with_loops.py

--- notebooks/playing_with_loops.py
+++ notebooks/playing_with_loops.py
@@ -1,12 +1,3 @@
 def draw_christmas_tree(height):
    
-    for i in range(height):
-        spaces = " " * (height - i - 1)
-        stars = "*" * (2 * i + 1)
-        print(spaces + stars)
-
-    for _ in range(2):
-        spaces = " " * (height - 2)
-        print(spaces + "###")
-
-
+ 
